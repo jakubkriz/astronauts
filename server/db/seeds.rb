b23d2bd9e0127ed68af55936b8c8534ee9801e6b
Jakub Kříž <jakubkriz92@gmail.com>
2017-07-23 23:47:56 +0200

    seeding data for server

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+astronauts = [
+  [ "John", "Doe", [1990, 2, 12], "invisibility"],
+  [ "Jack", "Jones", [1978, 11, 2], "healing"],
+  [ "David", "Dope", [1956, 4, 23], "strength"]
+]
+
+astronauts.each do |firstname, lastname, birthdate, superpower|
+  astronaut = Astronaut.
+    where(firstname: firstname).
+    where(lastname: lastname).
+    where(birthdate: Date.new(birthdate[0], birthdate[1], birthdate[2])).
+    where(superpower: superpower).first_or_initialize
+  astronaut.save!
+end
